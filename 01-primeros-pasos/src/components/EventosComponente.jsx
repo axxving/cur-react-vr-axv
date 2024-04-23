36671305ad8c01a812e8353af0eeb6fd9509244f
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-04-23 14:08:38 -0600

    feat(): onMouseEnter y onMouseLeave
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 01-primeros-pasos/src/components/EventosComponente.jsx
+++ 01-primeros-pasos/src/components/EventosComponente.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 
 const EventosComponente = () => {
-  // funcion de clic
   const hasDadodClic = (e, nombre) => {
-    alert("Haz dado clic a el boton " + nombre);
+    alert("Has dado clic al botón " + nombre);
   };
 
   const hasDadoDobleClic = (e, nombre) => {
-    alert("Has dado doble clic " + nombre);
+    alert("Has dado doble clic en " + nombre);
+  };
+
+  const hasEntrado = (accion) => {
+    alert(
+      `Has ${
+        accion === "entrado" ? "entrado en" : "salido de"
+      } la caja con el mouse`
+    );
   };
 
   return (
     <div>
       <h1>Eventos en React</h1>
       <p>
-        {/* Evento de Clic */}
         <button onClick={(e) => hasDadodClic(e, "Alex")}>Dame Clic</button>
       </p>
       <p>
-        {/* Evento de Doble Clic */}
         <button onDoubleClick={(e) => hasDadoDobleClic(e, "Alex")}>
           Dame Doble Clic
         </button>
+      </p>
+      <p>
+        <div
+          id="caja"
+          onMouseEnter={() => hasEntrado("entrado")}
+          onMouseLeave={() => hasEntrado("salido")}
+        >
+          Pasa por encima
+        </div>
       </p>
     </div>
   );
