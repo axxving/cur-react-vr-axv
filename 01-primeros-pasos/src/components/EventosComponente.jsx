0958c2adc876c5e4a72cdf1c5ba07987362b904e
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-04-23 14:20:12 -0600

    feat(): onFocus y onBlur
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 01-primeros-pasos/src/components/EventosComponente.jsx
+++ 01-primeros-pasos/src/components/EventosComponente.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const EventosComponente = () => {
-  const hasDadodClic = (e, nombre) => {
+  const hasDadoClic = (e, nombre) => {
     alert("Has dado clic al botón " + nombre);
   };
 
@@ -17,18 +17,26 @@ const EventosComponente = () => {
     );
   };
 
+  const estasDentro = (e) => {
+    alert("Estas dentro del input, introduce tu nombre");
+  };
+
+  const estasFuera = (e) => {
+    alert("Estas fuera del input");
+  };
+
   return (
     <div>
       <h1>Eventos en React</h1>
       <p>
-        <button onClick={(e) => hasDadodClic(e, "Alex")}>Dame Clic</button>
+        <button onClick={(e) => hasDadoClic(e, "Alex")}>Dame Clic</button>
       </p>
       <p>
         <button onDoubleClick={(e) => hasDadoDobleClic(e, "Alex")}>
           Dame Doble Clic
         </button>
       </p>
-      <p>
+      {/* <p>
         <div
           id="caja"
           onMouseEnter={() => hasEntrado("entrado")}
@@ -37,6 +45,15 @@ const EventosComponente = () => {
           Pasa por encima
         </div>
       </p>
+
+      <p>
+        <input
+          type="text"
+          onFocus={estasDentro}
+          onBlur={estasFuera}
+          placeholder="introduce tu nombre"
+        />
+      </p> */}
     </div>
   );
 };
