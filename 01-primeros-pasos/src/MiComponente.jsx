3a6016b8d3410bc4a3929ebe88a36eea8d754ee0
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-04-23 10:50:43 -0600

    feat(): pruebas en mi primer componente
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 01-primeros-pasos/src/MiComponente.jsx
+++ 01-primeros-pasos/src/MiComponente.jsx
@@ -3,9 +3,36 @@ import React from "react";
 
 // Funcion del componente
 const MiComponente = () => {
+  const nombre = "Victor";
+  let web = "victorroblesweb.es";
+
+  let usuario = {
+    nombre: "Alejandro",
+    apellidos: "Hernandez",
+    web: "alejandro-hrz.netlify.app",
+  };
+
   return (
     <>
       <p>Este es mi primer componente</p>
+      <h3>Datos del usuario:</h3>
+      <ul>
+        <li>Nombre: {nombre}</li>
+        <li>
+          Web: <strong> {web} </strong>
+        </li>
+        <hr />
+        <li>
+          Nombre: <strong>{JSON.stringify(usuario.nombre)}</strong>
+        </li>
+        <li>
+          Apellidos: <strong>{JSON.stringify(usuario.apellidos)}</strong>
+        </li>
+        <li>
+          Web: <strong>{JSON.stringify(usuario.web)}</strong>
+        </li>
+      </ul>
+
       <p>Estoy usando fragmentos</p>
     </>
   );
