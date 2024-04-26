6a1538da21ac46a22bd3a0165907174afc8fe56a
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-04-26 16:01:15 -0600

    feat():  componetizar
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 06-proyecto-01/src/App.js
+++ 06-proyecto-01/src/App.js
@@ -1,85 +1,52 @@
-import './App.css';
+import "./App.css";
+import Agregar from "./components/Agregar";
+import Buscador from "./components/Buscador";
+import Footer from "./components/Footer";
+import Listado from "./components/Listado";
 
 function App() {
   return (
     <div className="layout">
+      <header className="header">
+        <div className="logo">
+          <div className="play"></div>
+        </div>
 
-        <header className="header">
-            <div className="logo">
-                <div className="play"></div>
-            </div>
-            
-            <h1>MisPelis</h1>
-        </header>
+        <h1>MisPelis</h1>
+      </header>
 
-        <nav className="nav">
-            <ul>
-                <li><a href="/#">Inicio</a></li>
-                <li><a href="/#">Peliculas</a></li>
-                <li><a href="/#">Blog</a></li>
-                <li><a href="/#">Contacto</a></li>
-            </ul>
-        </nav>
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="/#">Inicio</a>
+          </li>
+          <li>
+            <a href="/#">Peliculas</a>
+          </li>
+          <li>
+            <a href="/#">Blog</a>
+          </li>
+          <li>
+            <a href="/#">Contacto</a>
+          </li>
+        </ul>
+      </nav>
 
-        <section id="content" className="content">
+      <section id="content" className="content">
+        {/*Aqui va el listado de peliculas*/}
+        <Listado />
+      </section>
 
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
+      <aside className="lateral">
+        {/* Aqui va el buscador */}
+        <Buscador />
 
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+        {/* Aqui va el componente de agregar */}
+        <Agregar />
+      </aside>
 
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-        </section>
-
-        <aside className="lateral">
-            <div className="search">
-                <h3 className="title">Buscador</h3>
-                <form>
-                    <input type="text" id="search_field" />
-                    <button id="search">Buscar</button>
-                </form>
-            </div>
-
-            <div className="add">
-                <h3 className="title">Añadir pelicula</h3>
-                <form>
-                    <input type="text" id="title" placeholder="Titulo" />
-                    <textarea id="description" placeholder="Descripción"></textarea>
-                    <input type="submit" id="save" value="Guardar" />
-                </form>
-            </div>
-        </aside>
-
-        <footer className="footer">
-            &copy; Máster en React - <a href="https://victorroblesweb.es">victorroblesweb.es</a>
-        </footer>
-
+      {/* Aqui va el footer*/}
+      <Footer />
     </div>
   );
 }
