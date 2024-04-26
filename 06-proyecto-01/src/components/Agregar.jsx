060bb3eeff080e95f5c9ba7577f908cb47bab755
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-04-26 17:19:20 -0600

    feat(): guardando en local storage
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 06-proyecto-01/src/components/Agregar.jsx
+++ 06-proyecto-01/src/components/Agregar.jsx
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Agregar = () => {
+  const tituloComponente = "";
+
+  const [pelistate, setPelistate] = useState({
+    titulo: "",
+    descripcion: "",
+  });
+
+  const { titulo, descripcion } = pelistate;
+
+  const conseguirDatosForm = (e) => {
+    e.preventDefault();
+
+    // conseguir datos del formulario
+    let target = e.target;
+    let titulo = target.titulo.value;
+    let descripcion = target.titulo.value;
+
+    // generando objeto de la pelicula a guardar
+    let peli = {
+      id: new Date().getTime(),
+      titulo,
+      descripcion,
+    };
+
+    setPelistate(peli);
+
+    // Guardar en el almacenamiento local
+    guardarEnStorage(peli);
+  };
+
+  const guardarEnStorage = (peli) => {
+    // Conseguir los elementos que ya tenemos en el local storage
+    let elementos = JSON.parse(localStorage.getItem("pelis"));
+    console.log(elementos);
+    // Comprobar si es un array
+    if (Array.isArray(elementos)) {
+      // Agregar un elemento nuevo
+      elementos.push(peli);
+    } else {
+      // Array con la nueva peli
+      elementos = [peli];
+    }
+
+    // guardar en el local storage
+    localStorage.setItem("pelis", JSON.stringify(elementos));
+
+    // Devolver un objeto
+    return peli;
+  };
+
   return (
     <>
       <div className="add">
-        <h3 className="title">Añadir pelicula</h3>
-        <form>
-          <input type="text" id="title" placeholder="Titulo" />
-          <textarea id="description" placeholder="Descripción"></textarea>
+        <h3 className="title">{tituloComponente}</h3>
+
+        <strong>
+          {titulo && descripcion && "Has generado la pelicula: " + titulo}
+        </strong>
+
+        <form onSubmit={conseguirDatosForm}>
+          <input type="text" id="titulo" name="titulo" placeholder="Titulo" />
+          <textarea
+            id="descripcion"
+            name="descripcion"
+            placeholder="Descripción"
+          ></textarea>
           <input type="submit" id="save" value="Guardar" />
         </form>
       </div>
