21a09b6ea0cacdbceb473e59a008c1815be11484
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-17 17:32:43 -0600

    feat(): tags in jsx

--- 06-componentes-basicos/01-primeros-pasos-cra/src/MiComponente.js
+++ 06-componentes-basicos/01-primeros-pasos-cra/src/MiComponente.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 export const MiComponente = () => {
     return (
-        <>
+        <div className='mi-comonente'>
             <hr />
-            <div>Este es mi primer componente en el curso de Victor Robles</div>
-        </>
+            <h2>Componente generado</h2>
+            <p>Este es mi primer componente en el curso de Victor Robles</p>
+            <ul>
+                <li>
+                    React
+                </li>
+                <li>
+                    Angular
+                </li>
+                <li>
+                    Vue
+                </li>
+            </ul>
+        </div>
     )
 }
