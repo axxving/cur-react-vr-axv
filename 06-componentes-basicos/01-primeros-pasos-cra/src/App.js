6f807465a1e22411b30b0328b51f29f0bde8aa88
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-17 17:27:04 -0600

    feat(): generating my first component

--- 06-componentes-basicos/01-primeros-pasos-cra/src/App.js
+++ 06-componentes-basicos/01-primeros-pasos-cra/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { MiComponente } from './MiComponente';
 
 function App() {
   return (
@@ -7,16 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido al master de React!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* cargar mi primer componente en el curso */}
+        <MiComponente />
+
       </header>
     </div>
   );
---
+++ 06-componentes-basicos/01-primeros-pasos-cra/src/MiComponente.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const MiComponente = () => {
+    return (
+        <>
+            <hr />
+            <div>Este es mi primer componente en el curso de Victor Robles</div>
+        </>
+    )
+}
