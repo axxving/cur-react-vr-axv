66e16ddb209debbb6e889c55668aa07ce8f353cb
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-17 18:06:16 -0600

    feat(): displaying component data in the view

--- 06-componentes-basicos/01-primeros-pasos-cra/src/MiComponente.js
+++ 06-componentes-basicos/01-primeros-pasos-cra/src/MiComponente.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 export const MiComponente = () => {
+
+    let usuario = {
+        nombre: "Alejandro",
+        apellidos: "Hernandez Ramirez",
+        web: "alex.ing.hz@gmail.com"
+    }
+
     return (
         <div className='mi-comonente'>
             <hr />
             <h2>Componente generado</h2>
+            <p>Datos del usuario:</p>
+            <ul>
+                <li>
+                    Nombre: <strong> {usuario.nombre} </strong>
+                </li>
+                <li>
+                    Apellidos: <strong> {usuario.apellidos} </strong>
+                </li>
+                <li>
+                    Web: <strong> {usuario.web} </strong>
+                </li>
+            </ul>
             <p>Este es mi primer componente en el curso de Victor Robles</p>
             <ul>
                 <li>
