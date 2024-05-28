2ccacf3f61d7666629e72032a200ba792e207746
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-28 17:43:01 -0600

    feat(): test with counter

--- 10-hooks/use-effect/src/components/PruebasComponent.js
+++ 10-hooks/use-effect/src/components/PruebasComponent.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 export const PruebasComponent = () => {
   const [usuario, setUsuario] = useState("Nombre");
+  const [contador, setContador] = useState(0);
 
   const modUsuario = (e) => {
     setUsuario(e.target.value);
@@ -17,6 +18,25 @@ export const PruebasComponent = () => {
     console.log("Se actualizo el usuario");
   }, [usuario]);
 
+  useEffect(() => {
+    if (contador >= 20) {
+      alert("Has llegado a 20");
+    }
+  }, [contador, usuario]);
+
+  const aumentar = () => {
+    setContador(contador + 1);
+    console.log("Haz dado clic al boton de aumentar: " + contador);
+  };
+
+  const decrementar = () => {
+    setContador(contador - 1);
+  };
+
+  const recet = () => {
+    setContador(0);
+  };
+
   return (
     <div>
       <strong className="nombre">{usuario}</strong>
@@ -26,6 +46,17 @@ export const PruebasComponent = () => {
           onChange={modUsuario}
           placeholder="Cambia el nombre"
         />
+        <br />
+        <h1 className={contador >= 10 ? "verde" : "rojo"}>{contador}</h1>
+        <button type="button" onClick={aumentar}>
+          Auemntar
+        </button>
+        <button type="button" onClick={decrementar}>
+          Decrementar
+        </button>
+        <button type="button" onClick={recet}>
+          Recetear
+        </button>
       </form>
     </div>
   );
