095387ceaacacdc223f366756a8a2987c11e931e
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-06-01 18:46:39 -0600

    feat(): ajax request

---
+++ 10-hooks/use-effect/src/components/AjaxComponent.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export const AjaxComponent = () => {
+  const [usuarios, setUsuarios] = useState([]);
+
+  const getUsuariosAjaxPms = () => {
+    fetch('https://reqres.in/api/users?page=1')
+      .then(respuesta => respuesta.json())
+      .then(resultadoFinal => {
+        setUsuarios(resultadoFinal.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    getUsuariosAjaxPms();
+  }, []);
+
+  return (
+    <>
+      <h2>Listado de usuarios via AJAX</h2>
+      <ul>
+        {
+          usuarios.map((usuario, index) => (
+            <li key={index}>
+              {usuario.first_name} {usuario.last_name}
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
+};
