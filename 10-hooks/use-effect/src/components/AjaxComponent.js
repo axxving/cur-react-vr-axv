5f92e7a88166725082d61817d1551ff83f1760cd
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-06-01 19:05:08 -0600

    feat(): loading message

--- 10-hooks/use-effect/src/components/AjaxComponent.js
+++ 10-hooks/use-effect/src/components/AjaxComponent.js
@@ -2,43 +2,56 @@ import { useEffect, useState } from "react";
 
 export const AjaxComponent = () => {
   const [usuarios, setUsuarios] = useState([]);
+  const [cargando, setCargando] = useState(true);
 
   const getUsuariosAjaxPms = () => {
     fetch('https://reqres.in/api/users?page=1')
       .then(respuesta => respuesta.json())
       .then(resultadoFinal => {
         setUsuarios(resultadoFinal.data);
+        setCargando(false);
       })
       .catch(error => {
         console.log(error);
+        setCargando(false);
       });
   }
 
   const getUsuariosAjaxAsyncAwait = async() => {
-    const peticion = await fetch("https://reqres.in/api/users?page=1");
-    const {data} = await peticion.json();
-
-    console.log(data);
-    setUsuarios(data);
+    try {
+      const peticion = await fetch("https://reqres.in/api/users?page=1");
+      const {data} = await peticion.json();
+      console.log(data);
+      setUsuarios(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setCargando(false);
+    }
   } 
 
   useEffect(() => {
-    //getUsuariosAjaxPms();
     getUsuariosAjaxAsyncAwait();
   }, []);
 
   return (
-    <>
-      <h2>Listado de usuarios via AJAX</h2>
-      <ul>
-        {
-          usuarios.map((usuario, index) => (
-            <li key={index}>
-              {usuario.first_name} {usuario.last_name}
-            </li>
-          ))
-        }
-      </ul>
-    </>
+    cargando === true ? (
+      <div className="cargando">
+        Cargando datos
+      </div>
+    ) : (
+      <>
+        <h2>Listado de usuarios via AJAX</h2>
+        <ul>
+          {
+            usuarios.map((usuario, index) => (
+              <li key={index}>
+                {usuario.first_name} {usuario.last_name}
+              </li>
+            ))
+          }
+        </ul>
+      </>
+    )
   );
 };
