935b9241068c7e970769cc3abea7d6deabc9c15b
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-06-02 15:56:41 -0600

    feat(): form to include movies

--- 04-proyecto01/src/components/Add.js
+++ 04-proyecto01/src/components/Add.js
@@ -1,14 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Add = () => {
+
+  const tituloComponente = "Agregar pelicula";
+
+  const [peliState, setPeliState] = useState({
+    titulo: "",
+    descripcion: "",
+  });
+
+  // Deesestructuracion de peliState
+  const {titulo, descripcion} = peliState;
+
+  const conseguirDatosForm = (e) => {
+    e.preventDefault();
+
+    // conseguir datos del formulario
+    let target = e.target;
+    let titulo = target.titulo.value;
+    let descripcion = target.descripcion.value;
+
+
+    console.log("Formulario enviado", "titulo: " + titulo + " descripcion: " + descripcion);
+
+    // generar objeto de la pelicula a guardar
+    let peli = {
+      id: new Date().getTime(),
+      titulo: titulo,
+      descripcion,
+    };
+
+    setPeliState(peli);
+
+    console.log(peli);
+  }
+
   return (
     <>
       <div class="add">
-        <h3 class="title">Añadir pelicula</h3>
-        <form>
-          <input type="text" id="title" placeholder="Titulo" />
-          <textarea id="description" placeholder="Descripción"></textarea>
-          <input type="submit" id="save" value="Guardar" />
+        <h3 class="title">{tituloComponente}</h3>
+
+        <strong>
+          {
+            (titulo && descripcion) && "Haz incluido la pelicula: " + titulo
+          }
+        </strong>
+        <form onSubmit={conseguirDatosForm}>
+          <input 
+            type="text" 
+            id="titulo" 
+            name="titulo"
+            placeholder="Titulo" 
+          />
+          <textarea 
+            id="descripcion"
+            name="descripcion" 
+            placeholder="Descripción" 
+          />
+          <input 
+            type="submit" 
+            id="save" 
+            value="Guardar" 
+          />
         </form>
       </div>
     </>
