0f97e7b56e1e5cbaa2aba290c533c397c656685a
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-06-06 18:37:35 -0600

    feat(): html of the form

--- 04-proyecto01/src/components/Editar.js
+++ 04-proyecto01/src/components/Editar.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
 export const Editar = () => {
+  const titulo_componente = "Editar un pelicula";
   return (
-    <div>Editar</div>
-  )
-}
+    <div className="edit_form">
+      <h3 className="title">{titulo_componente}</h3>
+      <form>
+        <input
+          type="text"
+          name="titulo"
+          className="titulo_editado"
+          defaultValue="Titulo original de la pelicula"
+        />
+        <textarea 
+          name="descripcion"
+          defaultValue="Decripcion original"
+          className="descripcion_editada"
+        />
+      </form>
+    </div>
+  );
+};
