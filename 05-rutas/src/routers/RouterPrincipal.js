a48faa11398cc9b21b8b06d91dea543f9d4f478c
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-06-07 08:05:52 -0600

    feat(): using navlink
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 05-rutas/src/routers/RouterPrincipal.js
+++ 05-rutas/src/routers/RouterPrincipal.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Contacto } from '../components/Contacto';
-import { Inicio } from '../components/Inicio';
-import { Articulos } from '../components/Articulos';
-import { Error } from '../components/Error';
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { Contacto } from "../components/Contacto";
+import { Inicio } from "../components/Inicio";
+import { Articulos } from "../components/Articulos";
+import { Error } from "../components/Error";
 
 export const RouterPrincipal = () => {
   return (
     <BrowserRouter>
+      <div>
+        <h2>Barra de navegacion</h2>
+
+        <hr />
+
+        <nav>
+          <li>
+            <NavLink to="/inicio">Inicio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/articulos">Articulos</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contacto">Contacto</NavLink>
+          </li>
+        </nav>
+      </div>
+
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/articulos" element={<Articulos />} />
-        <Route path="*" element={<Error />}/>
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
